fc09c68f2925f9077ff4608190267161b724cd95
Aayush Joglekar <aayushjog@gmail.com>
2020-10-14 16:27:00 +0530

    Added long polling for status

--- sample.cpp
+++ sample.cpp
@@ -8,3 +8,23 @@ int main()
     cout << "Hello world!\n";
     return 0;
 }
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+
+    int a = 0;
+
+    for(int i = 0; i < INT_MAX; i++) {
+
+        a++;
+
+    }
+
+    cout<<a<<'\'n';
+
+
+    return 0;
+}
--- src/App.js
+++ src/App.js
@@ -12,9 +12,11 @@ class App extends Component {
     super(props);
     this.state = {
       code: "",
+      currentRequest: null,
     };
     this.onChange = this.onChange.bind(this);
     this.submitCode = this.submitCode.bind(this);
+    this.pollForStatus = this.pollForStatus.bind(this);
   }
 
   onChange(currentCode, event) {
@@ -22,6 +24,38 @@ class App extends Component {
       code: currentCode,
     });
   }
+
+  pollForStatus = (count) => {
+    if (count > 20) {
+      alert("Timeout!");
+      return;
+    }
+
+    let taskData = this.state.currentRequest;
+
+    axios
+      .get("http://localhost:5100/status", {
+        params: { taskID: taskData["id"] },
+      })
+      .then(({ data }) => {
+        console.log(data);
+
+        this.setState({
+          currentRequest: data,
+        });
+
+        if (this.state.currentRequest["status"] !== 2) {
+          setTimeout(() => {
+            this.pollForStatus(count + 1);
+          }, 500);
+        } else {
+          console.log("Task complete!");
+        }
+      })
+      .catch((err) => {
+        throw err;
+      });
+  };
 
   submitCode() {
     const requestBody = JSON.stringify({
@@ -35,8 +69,13 @@ class App extends Component {
           "Content-Type": "application/json",
         },
       })
-      .then((response) => {
-        console.log(response);
+      .then(({ data }) => {
+        console.log(data);
+        this.setState({
+          currentRequest: data,
+        });
+        console.log(this.state);
+        this.pollForStatus(0);
       })
       .catch((err) => {
         throw err;
@@ -64,6 +103,26 @@ class App extends Component {
         />
         <div className="options-container">
           <button onClick={this.submitCode}>Submit</button>
+          <hr />
+          <div>
+            {this.state.currentRequest &&
+            this.state.currentRequest["status"] !== 2
+              ? "Running task"
+              : "Task Completed!"}
+          </div>
+          <div>
+            {this.state.currentRequest &&
+            this.state.currentRequest["status"] === 2
+              ? this.state.currentRequest["output"]
+              : ""}
+          </div>
+          <div>
+            {this.state.currentRequest &&
+            this.state.currentRequest["status"] === 2
+              ? (new Date(this.state.currentRequest["completedAt"]).getTime() -
+                new Date(this.state.currentRequest["startedAt"]).getTime()).toString() + "ms"
+              : ""}
+          </div>
         </div>
       </div>
     );
